900154b247747b580393846616d073d99e6923cf
Fred <6550969+MagicFred888@users.noreply.github.com>
2025-02-03 15:51:28 +0800

    Solver for day 01

--- AdventOfCode2020/Solver/day01.cs
+++ AdventOfCode2020/Solver/day01.cs
@@ -1,16 +1,30 @@
-﻿namespace AdventOfCode2020.Solver;
+﻿using AdventOfCode2020.Tools;
+
+namespace AdventOfCode2020.Solver;
 
 internal partial class Day01 : BaseSolver
 {
-    public override string PuzzleTitle { get; } = "XXXXXXXXXXXXXXXXXX";
+    public override string PuzzleTitle { get; } = "Report Repair";
 
     public override string GetSolution1(bool isChallenge)
     {
-        return "Answer for part 1";
+        List<int> numbers = QuickList.ListOfInt(_puzzleInput);
+        List<List<int>> results = SmallTools.GenerateCombinations(numbers.Count, 2).FindAll(n => numbers[n[0]] + numbers[n[1]] == 2020);
+        if (results.Count == 1)
+        {
+            return (numbers[results[0][0]] * numbers[results[0][1]]).ToString();
+        }
+        throw new InvalidDataException("No solution found !");
     }
 
     public override string GetSolution2(bool isChallenge)
     {
-        return "Answer for part 2";
+        List<int> numbers = QuickList.ListOfInt(_puzzleInput);
+        List<List<int>> results = SmallTools.GenerateCombinations(numbers.Count, 3).FindAll(n => numbers[n[0]] + numbers[n[1]] + numbers[n[2]] == 2020);
+        if (results.Count == 1)
+        {
+            return (numbers[results[0][0]] * numbers[results[0][1]] * numbers[results[0][2]]).ToString();
+        }
+        throw new InvalidDataException("No solution found !");
     }
 }
