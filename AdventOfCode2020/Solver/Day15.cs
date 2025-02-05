5d980d91b169c8efe77ab19ace3ec5f94ddbff2d
Fred <6550969+MagicFred888@users.noreply.github.com>
2025-02-05 21:49:36 +0800

    Solver for day 15

---
+++ AdventOfCode2020/Solver/Day15.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode2020.Solver;
+
+internal partial class Day15 : BaseSolver
+{
+    public override string PuzzleTitle { get; } = "Rambunctious Recitation";
+
+    public override string GetSolution1(bool isChallenge)
+    {
+        return PlayGame2(2020).ToString();
+    }
+
+    public override string GetSolution2(bool isChallenge)
+    {
+        return PlayGame2(30000000).ToString();
+    }
+
+    private long PlayGame2(int nbrOfTurn)
+    {
+        // Initialization
+        int[] game = new int[nbrOfTurn];
+        List<int> input = _puzzleInput[0].Split(',').ToList().ConvertAll(int.Parse);
+
+        // Record initial values
+        for (int i = 0; i < input.Count; i++)
+        {
+            game[input[i]] = i + 1;
+        }
+
+        // Run the game for remaining turns
+        int lastNumberSpoken = input[^1];
+        for (int turnId = input.Count; turnId < nbrOfTurn; turnId++)
+        {
+            int numberSpoken = game[lastNumberSpoken] == 0 ? 0 : turnId - game[lastNumberSpoken];
+            game[lastNumberSpoken] = turnId;
+            lastNumberSpoken = numberSpoken;
+        }
+
+        // Return last number spoken
+        return lastNumberSpoken;
+    }
+}
